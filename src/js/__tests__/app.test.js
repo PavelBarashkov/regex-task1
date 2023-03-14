6f1bb06d2208166bb2f778dec4d5f266b0a0c332
Pavel Barashkov <Pav.Barashkov@yandex.ru>
2023-03-14 16:29:34 +0400

    add eslint and appveyor.yml

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,13 +1,15 @@
-import Validator from "../app";
+/* eslint-disable linebreak-style */
+/* eslint-disable no-undef */
+import Validator from '../app';
 
 test('app test', () => {
-    const name = new Validator('paul-78_s');
-    const result = true;
-    expect(result).toBe(name.validateUsername());
-})
+  const name = new Validator('paul-78_s');
+  const result = true;
+  expect(result).toBe(name.validateUsername());
+});
 
 test('app test name1234', () => {
-    const name = new Validator('paul-7877_s');
-    const result = false;
-    expect(result).toBe(name.validateUsername());
-})
+  const name = new Validator('paul-7877_s');
+  const result = false;
+  expect(result).toBe(name.validateUsername());
+});
--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,13 @@
+/* eslint-disable linebreak-style */
 export default class Validator {
-    constructor(name) {
-        this.name = name
+  constructor(name) {
+    this.name = name;
+  }
+
+  validateUsername() {
+    if (/\d{3,}/.test(this.name)) {
+      return false;
     }
-    validateUsername() {
-        if (/\d{3,}/.test(this.name)) {
-            return false;
-        }
-         return /[^a-z][a-z0-9_-]*[a-z]$/.test(this.name);
-    }
+    return /[^a-z][a-z0-9_-]*[a-z]$/.test(this.name);
+  }
 }
