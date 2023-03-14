17990e75162dde76cab906ad4f2f659610d80d8f
Pavel Barashkov <Pav.Barashkov@yandex.ru>
2023-03-14 16:23:40 +0400

    init

---
+++ src/js/__tests__/app.test.js
@@ -0,0 +1,13 @@
+import Validator from "../app";
+
+test('app test', () => {
+    const name = new Validator('paul-78_s');
+    const result = true;
+    expect(result).toBe(name.validateUsername());
+})
+
+test('app test name1234', () => {
+    const name = new Validator('paul-7877_s');
+    const result = false;
+    expect(result).toBe(name.validateUsername());
+})
---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+export default class Validator {
+    constructor(name) {
+        this.name = name
+    }
+    validateUsername() {
+        if (/\d{3,}/.test(this.name)) {
+            return false;
+        }
+         return /[^a-z][a-z0-9_-]*[a-z]$/.test(this.name);
+    }
+}
